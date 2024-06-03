2ddc93b7ea3fe3a1d397cf69d071de7ac1f6942c
Dillon Steyl <dillon.steyl@snazzyshrimp.com>
2024-06-03 22:43:27 +1000

    chore: remove nested directory

--- ec2-instance/instance.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "app_server" {
-  ami           = "ami-076fe60835f136dc9"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "AnotherInstance"
-  }
-}
--- terraform.tf
+++ terraform.tf
@@ -11,3 +11,12 @@ terraform {
 provider "aws" {
   region = "ap-southeast-2"
 }
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-076fe60835f136dc9"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "AnotherInstance"
+  }
+}
