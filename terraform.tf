f02cc6e08dc0ac813fc20bf0d8fc581518dca30d
Dillon Steyl <dillon.steyl@snazzyshrimp.com>
2024-06-03 22:24:27 +1000

    add instance to top-level file

--- terraform.tf
+++ terraform.tf
@@ -7,3 +7,16 @@ terraform {
   }
   required_version = ">= 1.2.0"
 }
+
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-076fe60835f136dc9"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "MyInstance"
+  }
+}
